aefbd8030838d56d1b796cd88f369a0f7e197cb7
Miguel Lima <mglima1279@gmail.com>
2026-04-29 16:58:47 -0300

    refining security

--- src/main/java/com/bank/demo/controllers/AuthController.java
+++ src/main/java/com/bank/demo/controllers/AuthController.java
@@ -17,9 +17,10 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
+
     private final AuthService authService;
     private final JwtService jwtService;
-    
+
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody UserRequestDTO request) {
         try {
@@ -27,7 +28,7 @@ public class AuthController {
             String token = jwtService.generateToken(user);
             return ResponseEntity.ok(token);
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Error occurred while registering user");
+            return ResponseEntity.badRequest().body("Error occurred while registering user: " + e.getMessage());
         }
     }
 
